e871da7e4919d55e7e286b52141c754c04baf904
lewap <lewapek@gmail.com>
2016-05-03 14:39:37 +0200

    update

--- runner.py
+++ runner.py
@@ -1,5 +1,3 @@
-# import the necessary packages
-from collections import deque
 import argparse
 
 import cv2
@@ -7,27 +5,22 @@ import cv2
 from utils.utils import track_color
 
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video", help="path to the (optional) video file")
-ap.add_argument("-b", "--buffer", type=int, default=64, help="max buffer size")
-args = vars(ap.parse_args())
+args_parser = argparse.ArgumentParser()
+args_parser.add_argument("-v", "--video", help="path to the (optional) video file")
+args_parser.add_argument("-b", "--buffer", type=int, default=64, help="max buffer size")
+args = vars(args_parser.parse_args())
 
-# define the lower and upper boundaries of the "green" ball in the HSV color space, then initialize the
-# list of tracked points
+# colors ranges in format [lower_bound, upper_bound]
 green_color_range = [(29, 86, 6), (64, 255, 255)]
 red_color_range = [(0, 195, 205), (255, 255, 255)]
 colors = [green_color_range, red_color_range]
 max_buffer_length = args["buffer"]
-pts = deque(maxlen=max_buffer_length)
 
-# if a video path was not supplied, grab the reference to the webcam
 if not args.get("video", False):
     camera = cv2.VideoCapture(0)
 else:
     camera = cv2.VideoCapture(args["video"])
 
-track_color(camera=camera, pts=pts, colors=colors, max_buffer_size=max_buffer_length)
-
+track_color(camera=camera, colors=colors, max_buffer_size=max_buffer_length)
 camera.release()
 cv2.destroyAllWindows()
--- utils/utils.py
+++ utils/utils.py
@@ -1,74 +1,64 @@
+from collections import deque
 import cv2
 import imutils
 import numpy as np
 
 
-# colors - list of colors
-def track_color(camera, pts, colors, max_buffer_size, video_mode=False):
-
-    color_index = 0
+def track_color(camera, colors, max_buffer_size, video_mode=False):
     colors_length = len(colors)
+    points_list = [deque(maxlen=max_buffer_size) for _ in xrange(colors_length)]
     while True:
-        # setting current color
-        current_color = colors[color_index]
-        lower_color_bound = current_color[0]
-        upper_color_bound = current_color[1]
-        color_index = (color_index + 1) % colors_length
-        # print "Current color = " + str(current_color)
-
-        # grab the current frame
         (grabbed, frame) = camera.read()
 
-        # if we are viewing a video and we did not grab a frame,
-        # then we have reached the end of the video
         if video_mode and not grabbed:
             break
 
-        # print "frame=", frame
         # resize the frame, blur it, and convert it to the HSV color space
         frame = imutils.resize(frame, width=600)
         # blurred = cv2.GaussianBlur(frame, (11, 11), 0)
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-        # construct a mask for the color "green", then perform
-        # a series of dilations and erosions to remove any small
-        # blobs left in the mask
-        mask = cv2.inRange(hsv, lower_color_bound, upper_color_bound)
-        mask = cv2.erode(mask, None, iterations=2)
-        mask = cv2.dilate(mask, None, iterations=2)  # find contours in the mask and initialize the current
-        # (x, y) center of the ball
-        contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-        center = None
+        for color_index in xrange(colors_length):
+            current_color = colors[color_index]
+            lower_color_bound = current_color[0]
+            upper_color_bound = current_color[1]
+            points = points_list[color_index]
 
-        # only proceed if at least one contour was found
-        if len(contours) > 0:
-            # find the largest contour in the mask, then use
-            # it to compute the minimum enclosing circle and
-            # centroid
-            centroid = max(contours, key=cv2.contourArea)
-            ((x, y), radius) = cv2.minEnclosingCircle(centroid)
-            M = cv2.moments(centroid)
-            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+            # construct a mask for the color "green", then perform a series of dilations and erosions
+            # to remove any small blobs left in the mask
+            mask = cv2.inRange(hsv, lower_color_bound, upper_color_bound)
+            mask = cv2.erode(mask, None, iterations=2)
+            mask = cv2.dilate(mask, None, iterations=2)
+            # find contours in the mask and initialize the current (x, y) center of the ball
+            contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+            center = None
 
-            # only proceed if the radius meets a minimum size
-            if radius > 10:
-                # draw the circle and centroid on the frame,
-                # then update the list of tracked points
-                cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-                cv2.circle(frame, center, 5, (0, 0, 255), -1)
+            if len(contours) > 0:
+                # find the largest contour in the mask, then use it to compute the minimum
+                # enclosing circle and centroid
+                centroid = max(contours, key=cv2.contourArea)
+                ((x, y), radius) = cv2.minEnclosingCircle(centroid)
+                m = cv2.moments(centroid)
+                center = (int(m["m10"] / m["m00"]), int(m["m01"] / m["m00"]))
 
-        # update the points queue
-        pts.appendleft(center)  # loop over the set of tracked points
-        for i in xrange(1, len(pts)):
-            # if either of the tracked points are None, ignore
-            # them
-            if pts[i - 1] is None or pts[i] is None:
-                continue
+                # only proceed if the radius meets a minimum size
+                if radius > 10:
+                    # draw the circle and centroid on the frame, then update the list of tracked points
+                    cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+                    cv2.circle(frame, center, 5, lower_color_bound, -1)
 
-            # otherwise, compute the thickness of the line and
-            # draw the connecting lines
-            thickness = int(np.sqrt(max_buffer_size / float(i + 1)) * 2.5)
-            cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+            # update the points queue
+            points.appendleft(center)
+            # loop over the set of tracked points
+            for points_index in xrange(1, len(points)):
+                # if either of the tracked points are None, ignore them
+                if points[points_index - 1] is None or points[points_index] is None:
+                    continue
+
+                # otherwise, compute the thickness of the line and draw the connecting lines
+                thickness = int(np.sqrt(max_buffer_size / float(points_index + 1)) * 2.5)
+                cv2.line(frame, points[points_index - 1], points[points_index], lower_color_bound, thickness)
+        # end of colors loop
 
         # show the frame to our screen
         cv2.imshow("Frame", frame)
