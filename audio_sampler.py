3121a93605957b5d604a31cc7fe110bc2485b6a5
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2016-05-03 17:02:18 +0200

    WIP

--- audio_sampler.py
+++ audio_sampler.py
@@ -1,52 +1,96 @@
 import sys
 import pyaudio
 import wave
+import thread
 
 CHUNK_SIZE = 1024
 
-def play_sample(sample_filepath, chunk_size=CHUNK_SIZE):
-    '''
-    Play (on the attached system sound device) the WAV file
-    named sample_filepath.
-    '''
+SAMPLES = {
+    'snare': 'samples/snare.wav',
+    'tom':   'samples/tom.wav'
+}
 
-    try:
-        print 'Trying to play file ' + sample_filepath
-        wf = wave.open(sample_filepath, 'rb')
-    except IOError as ioe:
-        sys.stderr.write('IOError on file ' + sample_filepath + '\n' + \
-        str(ioe) + '. Skipping.\n')
-        return
-    except EOFError as eofe:
-        sys.stderr.write('EOFError on file ' + sample_filepath + '\n' + \
-        str(eofe) + '. Skipping.\n')
-        return
 
-    # Instantiate PyAudio.
-    p = pyaudio.PyAudio()
+class Sound:
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.channels = []
+        self.rate = None
+        self.samp_width = None
+        self.data = []
 
-    # Open stream.
-    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                    channels=wf.getnchannels(),
-                    rate=wf.getframerate(),
-                    output=True)
+        self.load()
 
-    data = wf.readframes(chunk_size)
-    while len(data) > 0:
-        stream.write(data)
-        data = wf.readframes(chunk_size)
+    def load(self):
+        print 'loading sample ' + self.filepath
+        sound_file = wave.open(self.filepath, 'rb')
+        print 'opened'
 
-    # Stop stream.
-    stream.stop_stream()
-    stream.close()
+        self.channels = sound_file.getnchannels()
+        self.rate = sound_file.getframerate()
+        self.samp_width = sound_file.getsampwidth()
+        print 'meta data collected'
 
-    # Close PyAudio.
-    p.terminate()
+        print 'reading...'
+        data = sound_file.readframes(CHUNK_SIZE)
+        while len(data) > 0:
+            # print 'reading...'
+            data = sound_file.readframes(CHUNK_SIZE)
+            self.data.append(data)
+
+
+class DrumSampler:
+    def __init__(self):
+        self.samples = {}
+        self.audio = pyaudio.PyAudio()
+
+        self.load_samples()
+
+    def load_samples(self):
+        for sample_name, sample_path in SAMPLES.iteritems():
+            self.samples[sample_name] = Sound(sample_path)
+
+    def play(self, sample_name):
+        print 'playing ' + sample_name
+        thread.start_new_thread(self.play_sample, (sample_name))
+
+    def play_sample(sample_name):
+        sample = self.samples[sample_name]
+
+        stream = audio.open(
+            format = audio.get_format_from_width(sample.width),
+            channels = sample.channels,
+            rate = sample.rate,
+            output = True
+        )
+
+        for data in sample.data:
+            stream.write(data)
+
+        stream.stop_stream()
+        stream.close()
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print 'usage: python audio_sampler.py sample_path'
-        exit(1)
+    print 'starting sampler'
+    drum_sampler = DrumSampler()
+    print 'started sampler'
 
-    play_sample(sys.argv[1])
+    drum_sampler.play('snare')
+    drum_sampler.play('snare')
+    drum_sampler.play('tom')
+    drum_sampler.play('snare')
+    drum_sampler.play('snare')
+    drum_sampler.play('tom')
+    drum_sampler.play('snare')
+    drum_sampler.play('snare')
+    drum_sampler.play('tom')
+    drum_sampler.play('snare')
+    drum_sampler.play('snare')
+    drum_sampler.play('tom')
+    drum_sampler.play('snare')
+    drum_sampler.play('snare')
+    drum_sampler.play('tom')
+
+    while(True):
+        pass
